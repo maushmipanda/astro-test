849b756eb579393c07dbe1d4226618cfe3f76c89
Maushmi <maushmi.work@gmail.com>
2024-07-29 19:01:19 +0100

    add file

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://maushmipanda.github.io',
+    base: 'astro-test',
+
+});
